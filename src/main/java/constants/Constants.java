8050b0a72f48621e3219a7278da4317cd87b9f2f
Misha Evdokimov <evdokimovmm1@yandex.ru>
2025-04-21 22:36:54 +0300

    Добавлена константы окружения и тестовых данных

---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class Constants {
+
+    //АПИ
+    public static String API_AUTH_REGISTER = "api/auth/register";
+    public static String API_AUTH_LOGIN = "api/auth/login";
+    public static String API_AUTH_USER = "api/auth/user";
+}
